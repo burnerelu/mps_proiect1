f4cfd4820c0d1528a7a65414b7f67fd87b0e1f37
Nicolae Alexandru <wenbsx@gmail.com>
2016-11-14 18:14:03 +0200

    Updated client

--- Client.java
+++ Client.java
@@ -7,24 +7,73 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+import javax.swing.*;
 import java.net.ConnectException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.InputStreamReader;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 
 
+
 public class Client {
+	 public static void sendMessage(PrintStream ps, String message, int code) {
+	        System.out.println("Sending " + message);
+	        ps.println("" + code + "<" + message + ">\n");
+	}
+	 
+	public static boolean all_ready(Player p1, Player[] players) { 
+		int ready_count = 0;
+		if (p1.ready_state == "Ready") 
+			ready_count++;
+		for(int i = 0; i < players.length; i++) {
+			if (players[i].ready_state == "Ready") 
+				ready_count++;
+		}
+		if (ready_count == 5)
+			return true;
+		else
+			return false;
+	}
+	
+	public static String encode_map(int[][] map) {
+		String separator = ", ";
+	    StringBuffer result = new StringBuffer();
+
+	    for (int i = 0; i < map.length; i++) {
+	        for(int j = 0; j < map[i].length; j++){
+	            result.append(map[i][j]);
+	            result.append(separator);
+	        }
+	        result.setLength(result.length() - separator.length());
+	        result.append("\n");
+	    }
+	    return result.toString();
+	}
+	
+	public static int[][] decode_map(String map) {
+		String[] lines = map.split("\n");
+		int[][] new_map = new int[40][40];
+		for (int i = 0; i < lines.length; i++) {
+			String[] columns = lines[i].split(",");
+			for (int j = 0; j < columns.length; j++) {
+				new_map[i][j] = Integer.parseInt(columns[j]);	
+			}
+		}
+		return new_map;
+	}
+	
 	static int Connected = 0;
     static String name = new String();
     static int attemp_conn = 0;
     static int leave_game = 0;
 	static int state_ready = 0;
+	static int game_started = 0;
     public static void main(String args[]) throws Exception {
     	/*
     	   	*Connection window
@@ -53,7 +102,7 @@ public class Client {
 				*/
 				
 				attemp_conn = 1;
-				name = userText.getText();
+				name = userText.getText().replaceAll("\n", "");
 				frame.setVisible(false);
 				frame.dispose();
 		      }
@@ -61,22 +110,17 @@ public class Client {
 		});
 
 		panel.add(connectButton);      
-		//frame.setVisible(true);
-		
-		
-
-		
-		
+		frame.setVisible(true);
 		
 		
         int rc;
         Socket sock = new Socket();
         while(Connected == 0) {
-        	System.out.println(name);
+        	System.out.println(attemp_conn);
         	if (attemp_conn == 1) {
         		try {
         			Connected = 1;
-        			sock = new Socket("localhost", 24999);
+        			sock = new Socket("10.3.16.68", 24999);
         		}
         		catch (ConnectException e) {
         			Connected = 0;
@@ -88,16 +132,13 @@ public class Client {
         /* Connected now */
         
         try {
-        	
+        	System.out.println("connected");
         	PrintStream pstream = new PrintStream(sock.getOutputStream());
             BufferedReader br =  new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            pstream.println(name);
-            
-            /*
-             * TODO: Parse messages pregame from server
-             * Server: nume:culoare:ready_status;nume:culoare:ready_status;
-             * When all players are ready: start game, sleep5 (to decide each player's position and state)
-             */
+            sendMessage(pstream, name, 200);
+            char[] inputChars = new char[1024];
+            String message;
+           
     		/*
         	 * Lobby window
         	 * TODO: Adapt each field to the middle of it's column
@@ -105,10 +146,7 @@ public class Client {
         	 * TODO: Make it pretty?
         	 */
     		
-    		/*
-    		 * Fields
-    		 */
-        	JFrame lobby_frame = new JFrame();
+            JFrame lobby_frame = new JFrame();
         	lobby_frame.setLayout(null);
         	lobby_frame.setLocationRelativeTo(null);
         	lobby_frame.setSize(450,350);
@@ -125,127 +163,158 @@ public class Client {
     		JLabel leaveLabel = new JLabel("Leave");
     		leaveLabel.setBounds(350, 10, 70, 10);
     		lobby_frame.add(leaveLabel);
-    		
-    		/*
-    		 * Players
-    		 */
-    		List<String> playerNames = new ArrayList<String>();
-    		/*
-    		 * 	playerNames.add("Bot1");
-    			playerNames.add("Bot2");
-    			playerNames.add("Bot3");
-    			playerNames.add("Bot4");
-    		*/
-    		JLabel player1Label = new JLabel(name);
-    		player1Label.setBounds(50, 60, 70, 10);
-    		lobby_frame.add(player1Label);
-    		JLabel player2Label = new JLabel(playerNames.get(0));
-    		player2Label.setBounds(50, 110, 70, 10);
-    		lobby_frame.add(player2Label);
-    		JLabel player3Label = new JLabel(playerNames.get(1));
-    		player3Label.setBounds(50, 160, 70, 10);
-    		lobby_frame.add(player3Label);
-    		JLabel player4Label = new JLabel(playerNames.get(2));
-    		player4Label.setBounds(50, 210, 70, 10);
-    		lobby_frame.add(player4Label);
-    		JLabel player5Label = new JLabel(playerNames.get(3));
-    		player5Label.setBounds(50, 260, 70, 10);
-    		lobby_frame.add(player5Label);
-    		
-    		List<String> colors = new ArrayList<String>();
-    		/*
-    			colors.add("Red");
-    			colors.add("Blue");
-    			colors.add("Green");
-    			colors.add("Purple");
-    			colors.add("Yellow");
-    		*/
-    		JLabel color1Label = new JLabel(colors.get(0));
-    		JLabel color2Label = new JLabel(colors.get(1));
-    		JLabel color3Label = new JLabel(colors.get(2));
-    		JLabel color4Label = new JLabel(colors.get(3));
-    		JLabel color5Label = new JLabel(colors.get(4));
-    		color1Label.setBounds(150, 60, 70, 10);
-    		color2Label.setBounds(150, 110, 70, 10);
-    		color3Label.setBounds(150, 160, 70, 10);
-    		color4Label.setBounds(150, 210, 70, 10);
-    		color5Label.setBounds(150, 260, 70, 10);
-    		lobby_frame.add(color1Label);
-    		lobby_frame.add(color2Label);
-    		lobby_frame.add(color3Label);
-    		lobby_frame.add(color4Label);
-    		lobby_frame.add(color5Label);
-    		
-    		/* Ready boxes
-    		 * 
-    		 */
-    		
-        	JCheckBox check = new JCheckBox("Ready");
-    		check.setBounds(250, 60, 70, 10);
-    		check.addActionListener(new ActionListener() {
-    			public void actionPerformed(ActionEvent e) {
-    				state_ready = 1;
-    				/*
-    				 * TODO Send ready state to server
-    				 */
-    				pstream.println(state_ready);
-    			}
-    		      
-    		});
-    		lobby_frame.add(check);
-    		
-    		List<String> playersReady = new ArrayList<String>();
-    		/*
-    			playersReady.add("Yes");
-    			playersReady.add("Yes");
-    			playersReady.add("Yes");
-    			playersReady.add("Yes");
-    		*/
-    		JLabel player1ReadyLabel = new JLabel(playersReady.get(0));
-    		JLabel player2ReadyLabel = new JLabel(playersReady.get(1));
-    		JLabel player3ReadyLabel = new JLabel(playersReady.get(2));
-    		JLabel player4ReadyLabel = new JLabel(playersReady.get(3));
-    		player1ReadyLabel.setBounds(250, 110, 70, 10);
-    		player2ReadyLabel.setBounds(250, 160, 70, 10);
-    		player3ReadyLabel.setBounds(250, 210, 70, 10);
-    		player4ReadyLabel.setBounds(250, 260, 70, 10);
-    		lobby_frame.add(player1ReadyLabel);
-    		lobby_frame.add(player2ReadyLabel);
-    		lobby_frame.add(player3ReadyLabel);
-    		lobby_frame.add(player4ReadyLabel);
-    		
-    		
-    		JButton leaveButton = new JButton("Leave");
-    		leaveButton.setBounds(350, 55, 70, 20);
-    		leaveButton.addActionListener(new ActionListener() {
-    			public void actionPerformed(ActionEvent e) {
-    				leave_game = 1;
-    			}
-    		});
-    		lobby_frame.add(leaveButton);
-    		
+    		JLabel redc = new JLabel("Red");
+    		redc.setBounds(150, 60, 70, 10);
+    		lobby_frame.add(redc);
+    		JLabel greenc = new JLabel("Green");
+    		greenc.setBounds(150, 110, 70, 10);
+    		lobby_frame.add(greenc);
+    		JLabel bluec = new JLabel("Blue");
+    		bluec.setBounds(150, 160, 70, 10);
+    		lobby_frame.add(bluec);
+    		JLabel yellowc = new JLabel("Yellow");
+    		yellowc.setBounds(150, 210, 70, 10);
+    		lobby_frame.add(yellowc);
+    		JLabel purplec = new JLabel("Purple");
+    		purplec.setBounds(150, 260, 70, 10);
+    		lobby_frame.add(purplec);
+		
     		lobby_frame.setVisible(true);
     		
+    		int[][] n = new int[][]{{50,60,70,10},{50, 110, 70, 10},{50, 160, 70, 10},{50, 210, 70, 10},{50, 260, 70, 10}};
+    		int[][] r = new int[][]{{250,60,70,10},{250, 110, 70, 10},{250, 160, 70, 10},{250, 210, 70, 10},{250, 260, 70, 10}};
+    		int[][] l = new int[][]{{350,60,70,10},{350, 110, 70, 10},{350, 160, 70, 10},{350, 210, 70, 10},{350, 260, 70, 10}};
+    		int red = 0;
+    		int gr = 1;
+    		int bl = 2;
+    		int yl = 3;
+    		int pp = 4;
     		
+    		Player me = new Player();
+    		me.set_name(name);
     		
-            
+    		Player[] players = new Player[4];
+    		for (int i = 0; i < 4; i++) {
+    			players[i] = new Player();
+    		}
+    		//Player player1 = new Player("test", "blue", "not ready", p, c, r, l);
+    		//player1.add_to_frame(lobby_frame, -1);
+    		
+
+    		while(game_started == 0 && leave_game == 0) {
+    			lobby_frame.revalidate();
+    			lobby_frame.repaint();
+    			
+    			message = br.readLine();
+       			message = message.split("<")[1].split(">")[0];
+    			String[] players_info = message.split(";");
+    			String[] my_info = players_info[0].split(":");
+    			if (me.name != my_info[0]) {
+    				me.set_name(my_info[0]);
+    			}
+    			if (me.color != my_info[1]) {
+    				me.set_color(my_info[1]);
+    			}
+    			String ready = "";
+    			if (Integer.parseInt(my_info[2]) == 1) {
+    				ready = "Ready"; 
+    			}
+    			else {
+    				ready = "Not ready";
+    			}
+    			int my_position = me.get_position(me.color);
+    			if (ready != "Ready") {
+    				JCheckBox check = new JCheckBox("Ready");
+            		check.setBounds(r[my_position][0], r[my_position][1], r[my_position][2], r[my_position][3]);
+            		check.addActionListener(new ActionListener() {
+            			public void actionPerformed(ActionEvent e) {
+            				state_ready = 1;
+            				sendMessage(pstream, "ready", 201); 
+            			}     		      
+            		});
+            		
+            		lobby_frame.add(check);
+    			}
+    			JButton leaveButton = new JButton("Leave");
+        		leaveButton.setBounds(l[my_position][0], l[my_position][1], l[my_position][2], l[my_position][3]);
+        		leaveButton.addActionListener(new ActionListener() {
+        			public void actionPerformed(ActionEvent e) {
+        				leave_game = 1;
+        				lobby_frame.setVisible(false);
+        				lobby_frame.dispose();
+        			}
+        		});
+        		lobby_frame.add(leaveButton);
+    			JLabel name_label = new JLabel(me.name);
+    			name_label.setBounds(n[my_position][0], n[my_position][1], n[my_position][2], n[my_position][3]);
+    			lobby_frame.remove(name_label);
+    			lobby_frame.add(name_label);
+    			
+    			for (int i = 1; i < players_info.length; i++) {
+    				int player_exists = 0;
+    				String[] info = players_info[i].split(":");
+    				Player player = new Player();
+    				player.set_name(info[0]);
+    				player.set_color(info[1]);
+    				int position = player.get_position(player.color);
+    				player.set_position(n[position], r[position], l[position]);
+    				if (Integer.parseInt(info[2]) == 1) {
+        				ready = "Ready";
+        			}
+        			else {
+        				ready = "Not ready";
+        			}
+    				player.set_state(ready);
+    				int j;
+    				for(j = 0; j < players.length; j++) {
+    					if (players[j].name == player.name) {
+    						players[i].set_state(player.ready_state);
+    						player.add_to_frame(lobby_frame);
+    						break;
+    					}
+    					if (players[j].name == "") {
+    						player.add_to_frame(lobby_frame);
+    						break;
+    					}
+    				}
+    				
+    			}
+    			
+    			if(all_ready(me, players)) {
+    				if (me.color == "Blue") {
+    					//String map_to_send = encode_map("Insert map here");
+    					//send_message(map);
+    				}
+    				else {
+    					message = br.readLine();
+    					//map = decode_map(message);
+    				}
+    				/*
+    				 * Send ready to server, all players ready
+    				 * sendMessage(pstream, "ready", 201); 
+    				 */
+    				game_started = 1;
+    				//start game
+    			}
+    		}
+    		
     		
             //pstream.println("Howdy, server!");
 
-            char[] inputChars = new char[1024];
+           
 
             //System.out.println("Trying to read");
             //br.read(inputChars);
             //System.out.println(inputChars);
             
             // Some things to keep the loop up
-            while(true && leave_game == 0) {
+            while(true) {
             	/*
             	 * TODO: send color, position, state, score
             	 * TODO: recv color, position, state
             	 */
-                rc = br.read(inputChars);
-                if(rc < 0) {
+                message = br.readLine();
+                if(message == null) {
                     System.out.println("Socket disconnected");
                     break;
                 }
@@ -258,4 +327,5 @@ public class Client {
         }
 
     }
+
 }
